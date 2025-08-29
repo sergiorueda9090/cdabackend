c29af5fbe9cd13ff297e4bb763c45e574ca513f2
sergiorueda9090 <sergiorueda90@hotmail.com>
2025-08-29 15:23:41 -0500

    websocket prueba@

--- backend/asgi.py
+++ backend/asgi.py
@@ -8,15 +8,23 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import cotizador.routing
-from cotizador.middleware import JWTAuthMiddlewareStack
 
+# Primero, establece la variable de entorno para las configuraciones de Django.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
+# Luego, inicializa la aplicación ASGI de Django.
+# Esto cargará las configuraciones de Django y el registro de aplicaciones.
+django_asgi_app = get_asgi_application()
+
+# Ahora que Django está configurado, puedes importar otros módulos
+# que dependen de las configuraciones, como tus middlewares y routings.
+from channels.routing import ProtocolTypeRouter, URLRouter
+import cotizador.routing
+from cotizador.middleware import JWTAuthMiddlewareStack # Ahora esta importación funcionará
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app, # Usa la aplicación ASGI de Django ya inicializada para HTTP
     "websocket": JWTAuthMiddlewareStack(
         URLRouter(
             cotizador.routing.websocket_urlpatterns
